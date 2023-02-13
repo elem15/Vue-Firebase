7884600096366048e6df9bc14f9c9c377b857e4b
Elem <elem15@yandex.ru>
2023-02-13 19:29:17 +0300

    feat: component with modal window

---
+++ modal-project/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>{{title}}</h1>
+  <div>
+    <input type="text" ref="name">
+    <button @click="handleEvent">Click me</button>
+  </div>
+  <p>{{text}}</p>
+  <Modal />
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',  
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'My first Vue App :)',
+      text: ''
+    }
+  },
+  methods: {
+    handleEvent() {
+      const input = this.$refs.name
+      console.log(input.value)
+      input.className = 'active';
+      input.focus();
+      this.text = input.value;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+.active {
+  border-color: green;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dark-drop">
+    <div class="modal">
+      <p class="content">modal content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.modal {
+  width: 400px;
+  height: 200px;
+  margin: 200px auto;
+  background: whitesmoke;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.dark-drop {
+  top: 0;
+  position: absolute;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+</style>
