92af9941a4482ed65b0c9189176b8eb4c4fd0d8a
Elem <elem15@yandex.ru>
2023-02-16 23:00:42 +0300

    feat: router with passing props

---
+++ router/src/views/jobs/JobView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>The job details</h1>
+    <h2>{{ getJob().title }}</h2>
+    <p>{{ getJob().details }}</p>
+  </div>
+</template>
+
+<script>
+import jobs from "../../assets/jobs.js";
+
+export default {
+  props: ["id"],
+  methods: {
+    getJob() {
+      return jobs.find((job) => job.id === +this.id);
+    },
+  },
+  mounted() {
+    console.log(this.getJob());
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ router/src/views/jobs/Jobs.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{ name: 'JobView', params: { id: job.id } }"
+      ><h2>{{ job.title }}</h2></router-link
+    >
+  </div>
+</template>
+
+<script>
+import jobs from "../../assets/jobs.js";
+export default {
+  data() {
+    return {
+      jobs,
+    };
+  },
+};
+</script>
+
+<style>
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+.job h2:hover {
+  background: #ddd;
+}
+.job a {
+  text-decoration: none;
+}
+</style>
