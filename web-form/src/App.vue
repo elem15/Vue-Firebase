004ec1ac1068ccdec4a458952584213c9d88d026
Elem <elem15@yandex.ru>
2023-02-16 12:52:38 +0300

    feat: simple form with v-model

---
+++ web-form/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <SignUp />
+</template>
+
+<script>
+import SignUp from "./components/SignUp.vue";
+
+export default {
+  name: "App",
+  components: {
+    SignUp,
+  },
+};
+</script>
+
+<style>
+body {
+  box-sizing: border-box;
+  margin: 0;
+  background-color: #eee;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
