94074a27a01ce155d3f488f9f3925379a7c51205
Elem <elem15@yandex.ru>
2023-02-20 23:20:15 +0300

    feat: refs & reactive

--- blog/src/views/HomeView.vue
+++ blog/src/views/HomeView.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p ref="p">My name is {{ name }}. I'm {{ age }} years old</p>
+    <h1>Ref</h1>
+    <p>My name is {{ first.name }}. I'm {{ first.age }} years old</p>
     <button @click="handleClick">Click me</button>
-    <button @click="age++">Increase age</button>
-    <input type="text" v-model="name" />
+    <button @click="first.age++">Increase age</button>
+    <input type="text" v-model="first.name" />
+    <h2>Reactive</h2>
+    <p>My name is {{ second.name }}. I'm {{ second.age }} years old</p>
+    <button @click="handleReactive">Click me</button>
+    <button @click="second.age++">Increase age</button>
+    <input type="text" v-model="second.name" />
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { reactive, ref } from "@vue/reactivity";
 // @ is an alias to /src
 
 export default {
   setup() {
-    const name = ref("Mario");
-    const age = ref(44);
+    const first = ref({ name: "Mario", age: 44 });
     const handleClick = () => {
-      name.value = "Nikita";
-      age.value = 33;
+      first.value.name = "Nikita";
+      first.value.age = 33;
+    };
+    const second = reactive({ name: "Romano", age: 35 });
+    const handleReactive = () => {
+      second.name = "Enrico";
+      second.age = 37;
     };
     return {
-      name,
-      age,
+      first,
+      second,
       handleClick,
+      handleReactive,
     };
   },
 };
