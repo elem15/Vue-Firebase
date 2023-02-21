43e3441743de9c54d0db6b19eb581f994aff6f2a
Elem <elem15@yandex.ru>
2023-02-21 13:00:44 +0300

    feat: watch & watchEffect

--- blog/src/views/HomeView.vue
+++ blog/src/views/HomeView.vue
@@ -1,28 +1,41 @@
 <template>
   <div class="home">
-    <input type="text" v-model="filterValue" />
+    <input type="text" v-model="search" />
     <div v-for="name in filteredNames" :key="name">{{ name }}</div>
+    <br />
+    <button @click="handleEvent">Stop watching</button>
   </div>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
-import { computed } from "@vue/runtime-core";
+import { computed, watch, watchEffect } from "@vue/runtime-core";
 // @ is an alias to /src
 
 export default {
   setup() {
-    const filterValue = ref("");
+    const search = ref("");
     const names = ref(["Mario", "Romul", "Teo", "Joi", "Amper"]);
     const filteredNames = computed(() => {
       return names.value.filter((name) =>
-        name.toLowerCase().includes(filterValue.value.toLowerCase())
+        name.toLowerCase().includes(search.value.toLowerCase())
       );
     });
+    const stopWatch = watch(search, () => {
+      console.log("watch");
+    });
+    const stopWatchEffect = watchEffect(() => {
+      console.log("watchEffect");
+    });
+    const handleEvent = () => {
+      stopWatch();
+      stopWatchEffect();
+    };
     return {
-      filterValue,
+      search,
       filteredNames,
       names,
+      handleEvent,
     };
   },
 };
