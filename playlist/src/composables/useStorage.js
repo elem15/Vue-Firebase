989ec56328aabdec3e88c3d3f56af7190758a74b
Elem <elem15@yandex.ru>
2023-03-08 14:11:50 +0300

    fix: not rewriting existing file in storage

--- playlist/src/composables/useStorage.js
+++ playlist/src/composables/useStorage.js
@@ -5,18 +5,24 @@ import getUser from './getUser'
 const useStorage = () => {
   const storageError = ref('')
   const filePath = ref('')
+
   const url = ref(null)
   const { user } = getUser()
   const uploadImage = async (file) => {
     filePath.value = `covers/${user.value.uid}/${file.name}`
     const storageRef = projectStorage.ref(filePath.value)
+    //if file name exist - don't rewriting and return old url
     try {
-      const res = await storageRef.put(file)
-      url.value = await res.ref.getDownloadURL()
-      error.value = ''
-    } catch (e) {
-      storageError.value = e.message === 'error is not defined' ? '' : e.message
-      console.log(storageError.value)
+      url.value = await storageRef.getDownloadURL()
+    } catch (err) {
+      try {
+        const res = await storageRef.put(file)
+        url.value = await res.ref.getDownloadURL()
+        storageError.value = ''
+      } catch (e) {
+        storageError.value = e.message === 'error is not defined' ? '' : e.message
+        console.log(storageError.value)
+      }
     }
   }
   const deleteImage = async (path) => {
